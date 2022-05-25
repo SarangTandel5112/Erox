25438e736719e46674ff1b0682f7ec5aa53bf394
SarangTandel5112 <sarangtandel5112@gmail.com>
2022-05-25 18:15:20 +0530

    file formatted

--- Backend/Logger/Logger.ts
+++ Backend/Logger/Logger.ts
@@ -17,7 +17,7 @@ class Logger {
     public logger: winston.Logger;
 
     constructor() {
-        
+
         this.logger = createLogger({
             format: combine(
                 label({ label: "E-Rox" }),
@@ -27,10 +27,6 @@ class Logger {
 
             defaultMeta: { service: "E-Rox Server" },
             transports: [
-                //
-                // - Write all logs with importance level of `error` or less to `error.log`
-                // - Write all logs with importance level of `info` or less to `combined.log`
-                //
                 new winston.transports.Console({ level: "info" }),
                 new winston.transports.File({ filename: "error.log", level: "error" }),
                 new winston.transports.File({ filename: "logs.log" }),
@@ -39,4 +35,5 @@ class Logger {
     }
 
 }
+
 export default Logger;
