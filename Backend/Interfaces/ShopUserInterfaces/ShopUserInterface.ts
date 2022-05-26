cbb0241793a5df2d3100cdfdb4da65d7282863fb
SarangTandel5112 <sarangtandel5112@gmail.com>
2022-05-26 13:01:16 +0530

    Shop model

--- Backend/Interfaces/ShopUserInterfaces/ShopUserInterface.ts
+++ Backend/Interfaces/ShopUserInterfaces/ShopUserInterface.ts
@@ -1,15 +1,15 @@
-import {Types , Document} from "mongoose";
+import { Types, Document } from "mongoose";
 interface ShopUserInterface extends Document {
-    firstName:String,
-    lastName:String,
-    shopId:Types.ObjectId,
-    shopAddress:Types.ObjectId,
-    password:String,
-    emailId:String,
-    email_verified:Boolean,
-    userName:String,
-    accessToken:String,
-    bankAccountId:Types.ObjectId,
-    phoneNumber:Number
+    firstName: String,
+    lastName: String,
+    shopId: Types.ObjectId,
+    shopAddress: Types.ObjectId,
+    password: String,
+    emailId: String,
+    email_verified: Boolean,
+    userName: String,
+    accessToken: String,
+    bankAccountId: Types.ObjectId,
+    phoneNumber: Number
 }
 export default ShopUserInterface;
--- Backend/Models/ShopUserModels/ShopUser.ts
+++ Backend/Models/ShopUserModels/ShopUser.ts
@@ -1,61 +1,61 @@
-import { Schema ,model} from "mongoose";
+import { Schema, model } from "mongoose";
 import ShopUserInterface from "../../Interfaces/ShopUserInterfaces/ShopUserInterface";
 
 
 
 const shopUserSchema = new Schema<ShopUserInterface>({
-    firstName:{
-        type:String,
-        required:true
+    firstName: {
+        type: String,
+        required: true
 
     },
-    lastName:{
-        type:String,
-        required:true,
+    lastName: {
+        type: String,
+        required: true,
     },
-    emailId:{
-        type:String,
-        required:true,
-        unique:true
+    emailId: {
+        type: String,
+        required: true,
+        unique: true
 
     },
-    userName:{
-        type:String,
-        required:true,
-        unique:true
+    userName: {
+        type: String,
+        required: true,
+        unique: true
 
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
 
     },
-    phoneNumber:{
-        type:Number,
-        required:true
+    phoneNumber: {
+        type: Number,
+        required: true
 
     },
-    bankAccountId:{
-        type:Schema.Types.ObjectId,
-        ref:"BankAccount"
+    bankAccountId: {
+        type: Schema.Types.ObjectId,
+        ref: "BankAccount"
     },
-    shopAddress:{
-        type:Schema.Types.ObjectId,
-        ref:"ShopAdress",
-        required:true
+    shopAddress: {
+        type: Schema.Types.ObjectId,
+        ref: "ShopAdress",
+        required: true
     },
-    shopId:{
-        type:Schema.Types.ObjectId
+    shopId: {
+        type: Schema.Types.ObjectId
     },
-    email_verified:{
-        type:Boolean,
-        default:false
+    email_verified: {
+        type: Boolean,
+        default: false
     },
-    accessToken:{
+    accessToken: {
         type: String
     },
-},{timestamps:true});
+}, { timestamps: true });
 
-const ShopUser=model<ShopUserInterface>("ShopUser",shopUserSchema);
+const ShopUser = model<ShopUserInterface>("ShopUser", shopUserSchema);
 
 export default ShopUser;
